507b200df69abeb669a00d765e473bc8feda10ca
arkasandr <arkasandr@gmail.com>
2018-03-18 23:23:39 +0300

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,34 @@
+package ru.job4j;
+
+/**
+* CalculateTest.
+*
+* @author Alex Arks (arkaleks@yandex.ru)
+*/
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Alex Arks (arkaleks@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/ @Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Alex Arks";
+    String expect = "Echo, echo, echo : Alex Arks"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
