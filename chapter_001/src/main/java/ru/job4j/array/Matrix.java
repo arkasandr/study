918df41216c32e8a4b634d5ff5fd87bd82b88af3
arkasandr <arkasandr@gmail.com>
2018-04-06 22:34:12 +0300

    5.4. Двухмерный массив. Таблица умножения. [#33491][#47870]_ver.2

--- chapter_001/src/main/java/ru/job4j/array/Matrix.java
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -11,47 +11,20 @@ import java.util.Arrays;
 public class Matrix {
 	
 	public static int[][] multiple(int size) {
-		StringBuilder screen = new StringBuilder();
+
 		String ln = System.lineSeparator();
 
-		int[][] data = new int[size+1][size+1];
+		int[][] data = new int[size][size];
 
-		System.out.printf("  *|");
-
-		for (int i = 1; i <= size; i++) {
-			System.out.printf(" %2d|", i);
-		}
-		System.out.printf(ln);
-		System.out.printf("    ");
-;
-		for (int i = 1; i <= size; i++) {
-			System.out.printf("----", i);
-		}
-		System.out.printf(ln);
-
-
-		for (int i = 1; i <= size; i++) {
-				System.out.printf(" %2d|", i);
-
-			for (int j = 1; j <= size; j++) {
-
-				System.out.printf(" %2d|", data[i][j] = i * j);
-
-
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					data[i][j] = (i + 1) * (j + 1);
 			}
 				System.out.printf(ln);
 		}
-		System.out.printf("    ");
-		for (int i = 1; i <= size; i++) {
-			System.out.printf("----", i);
-		}
-		System.out.printf(ln);
 		return data;
 	}
-
 	        public static void main(String[] args) {
-			Matrix m = new Matrix();
-
-				System.out.println(multiple(9));
+			System.out.println(Arrays.deepToString(multiple(2)));
 		}
 }
