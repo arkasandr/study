03e8baf77d1be17ac13f8a88e9879c6e788078b8
arkasandr <arkasandr@gmail.com>
2018-04-23 10:08:35 +0300

    2. Реализовать класс Tracker [#396]_item

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+public class Item {
+    public String name;
+
+    public String description;
+
+    public long create;
+
+    private String id;
+
+    public Item() {
+
+    }
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Item(String name, String description, long create) {
+        this.name = name;
+        this.description = description;
+        this.create = create;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public long getCreate() {
+        return this.create;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
