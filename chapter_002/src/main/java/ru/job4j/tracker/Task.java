33aedae06723cdb761772b6e4a71b5be0bc11c52
arkasandr <arkasandr@gmail.com>
2018-09-27 22:46:01 +0300

    3. Конвертация ArrayList в двухмерный массив [#10035][#62988]

--- chapter_002/src/main/java/ru/job4j/tracker/Task.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.tracker;
-
-public class Task extends Item {
-    public Task(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-}
