bfacc884ab12f82656fa8aff39d60a1e7f2bafcf
arkasandr <arkasandr@gmail.com>
2018-10-03 17:53:14 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/search/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/search/SortUser.java
@@ -3,7 +3,12 @@ package ru.job4j.search;
 import java.util.*;
 
 public class SortUser {
-
+    /**
+     * Метод сортирует список пользователей.
+     * В классе User переопределен метод compareTo().
+     *
+     * @return Упорядоченный набор пользователей (по возрастанию age).
+     */
 
                 public Set<User> sort(List<User> input) {
                     Set<User> result = new TreeSet<>();
@@ -12,4 +17,44 @@ public class SortUser {
                     }
                     return result;
                 }
-            }
+
+    /**
+     * Метод сортирует список пользователей.
+     * Использует Comparator.
+     * @return Упорядоченный набор пользователей по длине имени.
+     */
+
+    public List<User> sortNameLength(List<User> users) {
+                    Collections.sort(users,
+                    new Comparator<User>() {
+                        @Override
+                        public int compare(User o1, User o2) {
+                            return o1.getName().length() - o2.getName().length();
+                        }
+                    }
+                    );
+                    return users;
+                }
+
+
+    /**
+     * Метод сортирует список пользователей.
+     * Использует Comparator.
+     * @return Упорядоченный набор пользователей по лексикографическому значению и возрасту.
+     */
+
+     public List<User> sortByAllFields(List<User> users) {
+        Collections.sort(users,
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int rsl = o1.getName().compareTo(o2.getName());
+                        return rsl != 0 ? rsl : Integer.compare(o1.getAge(), o2.getAge());
+                    }
+                }
+        );
+        return users;
+    }
+
+
+}
--- chapter_003/src/test/java/ru/job4j/search/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/search/SortUserTest.java
@@ -29,31 +29,52 @@ public class SortUserTest {
         );
         assertThat(result, is(expect));
     }
+
+
+    @Test
+    public void sortUsersByNameLength() {
+        List<User> input = new ArrayList<>();
+        input.add(new User("Tom", 7));
+        input.add(new User("Jane", 6));
+        input.add(new User("Albert", 4));
+        input.add(new User("Jane", 17));
+        input.add(new User("Bob", 10));
+        SortUser d = new SortUser();
+
+        List<String> result = new ArrayList<>();
+        for (User a : d.sortNameLength(input)) {
+            result.add(a.getName());
+        }
+
+        List<String> expect = Arrays.asList(
+                "Tom", "Bob", "Jane", "Jane", "Albert"
+        );
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void sortUsersByNameFirstThenByAge() {
+        List<User> input = new ArrayList<>();
+        input.add(new User("Tom", 7));
+        input.add(new User("Jane", 6));
+        input.add(new User("Albert", 4));
+        input.add(new User("Jane", 17));
+        input.add(new User("Bob", 10));
+        SortUser b = new SortUser();
+
+        List<String> result = new ArrayList<>();
+        for (User a : b.sortByAllFields(input)) {
+            result.add(a.getName() + ", " + a.getAge());
+        }
+
+        List<String> expect = Arrays.asList(
+                "Albert, 4", "Bob, 10", "Jane, 6", "Jane, 17", "Tom, 7"
+        );
+        assertThat(result, is(expect));
+    }
+
+
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//                List<User> input = new ArrayList<>();
-//                input.add(new User("Tom", 7));
-//                input.add(new User("Jane", 6));
-//                input.add(new User("Albert", 4));
-//                input.add(new User("Jane", 17));
-//                SortUser result = new SortUser();
-//
-//                for(User o: result.sort(input)) {
-//                    System.out.println(o.name + " " + o.age);
-//                }
-//            }
-//        }
