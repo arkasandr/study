c59896686febeff948f5f6f87260995133196fdf
arkasandr <arkasandr@gmail.com>
2018-11-25 00:26:13 +0300

    1. Подсчет функции в диапазоне. [#24251]_ver.2

--- chapter_004/src/test/java/ru/job4j/lambda/DiapasonFunctionTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/DiapasonFunctionTest.java
@@ -9,26 +9,26 @@ import static org.junit.Assert.assertThat;
 
 public class DiapasonFunctionTest {
     @Test
-    public void whenLinearThen579(){
+    public void whenLinearThen579() {
         DiapasonFunction linear = new DiapasonFunction();
-        List<Double> result = linear.diapason(1,4,
-                x -> x*2 +3);
+        List<Double> result = linear.diapason(1, 4,
+                x -> x * 2 + 3);
         assertThat(result, is(Arrays.asList(5D, 7D, 9D)));
     }
 
     @Test
-    public void whenQuadraticThen51119(){
+    public void whenQuadraticThen51119() {
         DiapasonFunction linear = new DiapasonFunction();
-        List<Double> result = linear.diapason(1,4,
-                x -> x*x + 3*x + 1);
+        List<Double> result = linear.diapason(1, 4,
+                x -> x * x + 3 * x + 1);
         assertThat(result, is(Arrays.asList(5D, 11D, 19D)));
     }
 
     @Test
-    public void whenCubicThen52053(){
+    public void whenCubicThen52053() {
         DiapasonFunction linear = new DiapasonFunction();
-        List<Double> result = linear.diapason(1,4,
-                x -> x*x*x +3*x*x - x + 2);
+        List<Double> result = linear.diapason(1, 4,
+                x -> x * x * x + 3 * x * x - x + 2);
         assertThat(result, is(Arrays.asList(5D, 20D, 53D)));
     }
 }
