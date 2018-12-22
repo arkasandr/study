fe42614b5095541d54b29ffde452c493e70bbb27
arkasandr <arkasandr@gmail.com>
2018-12-22 21:01:01 +0300

    1. Singleton. [#94606]_ver.2

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleOneTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleOneTest.java
@@ -2,8 +2,6 @@ package ru.job4j.tracker;
 
 
 import org.junit.Test;
-import java.util.Arrays;
-import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,70 +9,9 @@ import static org.junit.Assert.assertThat;
     public class TrackerSingleOneTest {
 
         @Test
-        public void whenFindByIdThenTrackerGiveItem() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item item = new Item("test1", "testDescription", 123L);
-            tracker.add(item);
-            item.setId("123");
-            assertThat(tracker.findById(id -> id.equals(id)).getName(), is("test1"));
-        }
-
-        @Test
-        public void whenFindAllThenTrackerGiveItems() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item item1 = new Item("test1", "testDescription", 123L);
-            Item item2 = new Item("test2", "testDescription", 123L);
-            tracker.add(item1);
-            tracker.add(item2);
-            assertThat(tracker.findAll().size(), is(2));
-        }
-
-        @Test
-        public void whenAddNewItemThenTrackerHasSameItem() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item item = new Item("test1", "testDescription", 123L);
-            tracker.add(item);
-            assertThat(tracker.findAll(), is(Arrays.asList(item)));
-        }
-
-        @Test
-        public void whenFindItemByNameThenTrackerHasNameList() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item item1 = new Item("test1", "testDescription", 123L);
-            Item item2 = new Item("test1", "testDescription", 123L);
-            tracker.add(item1);
-            tracker.add(item2);
-            List<Item> result = Arrays.asList(item1, item2);
-            assertThat(tracker.findByName(name -> name.equals("test1")), is(result));
-        }
-
-        @Test
-        public void whenDeleteThisItemThenTrackerHasNull() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item deleted1 = new Item("test1", "testDescription1", 123L);
-            Item deleted2 = new Item("test2", "testDescription2", 123L);
-            Item deleted3 = new Item("test3", "testDescription3", 123L);
-            tracker.add(deleted1);
-            tracker.add(deleted2);
-            tracker.add(deleted3);
-            tracker.delete(deleted2.getId());
-            List<Item> result = Arrays.asList(deleted1, deleted3);
-            assertThat(tracker.findAll(), is(result));
-        }
-
-        @Test
-        public void whenReplaceNameThenReturnNewName() {
-            TrackerSingleOne tracker = TrackerSingleOne.INSTANCE;
-            Item previous = new Item("test1", "testDescription", 123L);
-            tracker.add(previous);
-            Item next = new Item("test2", "testDescription2", 1234L);
-            next.setId(previous.getId());
-            tracker.replace(previous.getId(), next);
-            assertThat(tracker.findById(id -> id.equals(id)).getName(), is("test2"));
-        }
-
-        @Test
         public void whenDoubleSingleOne() {
-            assertThat(TrackerSingleOne.INSTANCE, is(TrackerSingleOne.INSTANCE));
+            TrackerSingleOne firstTracker = TrackerSingleOne.INSTANCE;
+            TrackerSingleOne secondTracker = TrackerSingleOne.INSTANCE;
+            assertThat(firstTracker == secondTracker, is(true));
         }
     }
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleTwoTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSingleTwoTest.java
@@ -1,89 +1,18 @@
 package ru.job4j.tracker;
 
-//import org.junit.Before;
-import org.junit.Test;
 
-//import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
+import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 
 public class TrackerSingleTwoTest {
 
-//    @Before
-//    public void resetSingleton() throws SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
-//        Field INSTANCE = TrackerSingleTwo.class.getDeclaredField("instance");
-//        INSTANCE .setAccessible(true);
-//        INSTANCE .set(null, null);
-//    }
-
-    @Test
-    public void whenFindByIdThenTrackerGiveItem() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item item = new Item("test1", "testDescription", 123L);
-        tracker.add(item);
-        item.setId("123");
-        assertThat(tracker.findById(id -> id.equals(id)).getName(), is("test1"));
-    }
-    @Test
-    public void whenFindAllThenTrackerGiveItems() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item item1 = new Item("test1", "testDescription", 123L);
-        Item item2 = new Item("test2", "testDescription", 123L);
-        tracker.add(item1);
-        tracker.add(item2);
-        assertThat(tracker.findAll().size(), is(2));
-    }
-
-    @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item item = new Item("test1", "testDescription", 123L);
-        tracker.add(item);
-        assertThat(tracker.findAll(), is(Arrays.asList(item)));
-    }
-
-    @Test
-    public void whenFindItemByNameThenTrackerHasNameList() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item item1 = new Item("test1", "testDescription", 123L);
-        Item item2 = new Item("test1", "testDescription", 123L);
-        tracker.add(item1);
-        tracker.add(item2);
-        List<Item> result = Arrays.asList(item1, item2);
-        assertThat(tracker.findByName(name -> name.equals("test1")), is(result));
-    }
-
-    @Test
-    public void whenDeleteThisItemThenTrackerHasNull() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item deleted1 = new Item("test1", "testDescription1", 123L);
-        Item deleted2 = new Item("test2", "testDescription2", 123L);
-        Item deleted3 = new Item("test3", "testDescription3", 123L);
-        tracker.add(deleted1);
-        tracker.add(deleted2);
-        tracker.add(deleted3);
-        tracker.delete(deleted2.getId());
-        List<Item> result = Arrays.asList(deleted1, deleted3);
-        assertThat(tracker.findAll(), is(result));
-    }
-
-    @Test
-    public void whenReplaceNameThenReturnNewName() {
-        TrackerSingleTwo tracker = TrackerSingleTwo.getInstance();
-        Item previous = new Item("test1", "testDescription", 123L);
-        tracker.add(previous);
-        Item next = new Item("test2", "testDescription2", 1234L);
-        next.setId(previous.getId());
-        tracker.replace(previous.getId(), next);
-        assertThat(tracker.findById(id -> id.equals(id)).getName(), is("test2"));
-    }
-
     @Test
     public void whenDoubleSingleTwo() {
-        assertThat(TrackerSingleTwo.getInstance(), is(TrackerSingleTwo.getInstance()));
+        TrackerSingleTwo firstTracker = TrackerSingleTwo.getInstance();
+        TrackerSingleTwo secondTracker = TrackerSingleTwo.getInstance();
+        assertThat(firstTracker == secondTracker, is(true));
     }
 }
 
