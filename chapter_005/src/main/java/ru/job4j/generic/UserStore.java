d38487bdbfc5979a6759f79c47f7455cec4b6cef
arkasandr <arkasandr@gmail.com>
2019-01-12 23:37:41 +0300

    5.2.2. Реализовать Store<T extends Base>[#104912]

--- chapter_005/src/main/java/ru/job4j/generic/UserStore.java
+++ chapter_005/src/main/java/ru/job4j/generic/UserStore.java
@@ -1,58 +1,8 @@
 package ru.job4j.generic;
 
-public class UserStore<User extends Base> implements Store<User>  {
-   private SimpleArray<User> users;
+public class UserStore extends AbstractStore<User>  {
 
     public UserStore(int size) {
-        users = new SimpleArray<>(size);
-    }
-
-    @Override
-    public void add(User model) {
-        users.add(model);
-    }
-
-    @Override
-    public boolean replace(String id, User model) {
-        boolean result = false;
-        for (int i = 0; i < users.getSize(); i++) {
-                if (users.get(i) != null && users.get(i).getId().equals(id)) {
-                    users.set(i, model);
-                    result = true;
-                    break;
-                }
-            }
-        return result;
-    }
-
-
-    @Override
-    public boolean delete(String id) {
-        boolean result = false;
-        for (int i = 0; i < users.getSize(); i++) {
-                if (users.get(i) != null && users.get(i).getId().equals(id)) {
-                    users.remove(i);
-                    result = true;
-                    break;
-                }
-        }
-        return result;
-    }
-
-    @Override
-    public User findById(String id) {
-        User result = null;
-        for (int i = 0; i < users.getSize(); i++) {
-                if (users.get(i) != null && users.get(i).getId().equals(id)) {
-                    result = users.get(i);
-                    break;
-                }
-            }
-        return result;
-    }
-
-
-    public User get(int position) {
-        return users.get(position);
+        super(size);
     }
 }
