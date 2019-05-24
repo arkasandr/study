f6d6f3f3cc6b8092ed3f7ca34741a75d041c1229
arkasandr <arkasandr@gmail.com>
2019-05-24 06:06:48 +0300

    2. Фильтры [#1732]_ver.2

--- chapter_007/src/main/java/ru/job4j/query/goods.sql
+++ chapter_007/src/main/java/ru/job4j/query/goods.sql
@@ -10,27 +10,26 @@ id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
 	type_id INTEGER REFERENCES types(id),
 	expired_date DATE,
-	price NUMERIC,
-	quantity INTEGER
+	price NUMERIC
 );
 
 INSERT INTO types (name) VALUES ('сыр'), ('молоко'), ('мясо'), ('мороженое');
-INSERT INTO products (name, type_id, expired_date, price, quantity) VALUES
-('сыр Гауда', 1, '2019-05-24', 650, 5),
-('сыр Бри', 1, '2019-06-30', 1300, 1),
-('молоко козье', 2, '2019-05-23', 90, 26),
-('молоко 3,2%', 2, '2019-05-24', 56, 41),
-('молоко овсяное', 2, '2019-07-16', 80, 12),
-('мясо свежее (говядина)', 3, '2019-06-27', 560, 4),
-('мясо мороженое (утка)', 3, '2019-07-21', 480, 3),
-('мороженое ваниль', 4, '2019-08-01', 64, 14),
-('мороженое шоколад', 4, '2019-07-23', 72, 8);
+INSERT INTO products (name, type_id, expired_date, price) VALUES
+('сыр Гауда', 1, '2019-05-24', 650),
+('сыр Бри', 1, '2019-06-30', 1300),
+('молоко козье', 2, '2019-05-23', 90),
+('молоко 3,2%', 2, '2019-05-24', 56),
+('молоко овсяное', 2, '2019-07-16', 80),
+('мясо свежее (говядина)', 3, '2019-06-27', 560),
+('мясо мороженое (утка)', 3, '2019-07-21', 480),
+('мороженое ваниль', 4, '2019-08-01', 64),
+('мороженое шоколад', 4, '2019-07-23', 72);
 
 --1. Написать запрос получения всех продуктов с типом "СЫР"
-SELECT name, price, quantity FROM products WHERE type_id=1;
+SELECT name, price FROM products WHERE type_id=(SELECT id FROM types WHERE name = 'сыр');
 
 --2. Написать запрос получения всех продуктов, у кого в имени есть слово "мороженное"
-SELECT name, price, quantity FROM products WHERE name LIKE '%мороженое%';
+SELECT name, price FROM products WHERE name LIKE '%мороженое%';
 
 --3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 SELECT name, price, expired_date FROM products WHERE date_part('month', expired_date) - date_part('month', now()) = 1;
@@ -39,13 +38,13 @@ SELECT name, price, expired_date FROM products WHERE date_part('month', expired_
 SELECT name, price FROM products WHERE price = (SELECT MAX(price) FROM products);
 
 --5. Написать запрос, который выводит количество всех продуктов определенного типа.
-SELECT name, price, quantity FROM products WHERE type_id=(SELECT id FROM types WHERE name = 'мясо');
+SELECT COUNT(name) FROM products WHERE type_id=(SELECT id FROM types WHERE name = 'молоко');
 																								  
 --6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-SELECT name, price, quantity FROM products WHERE type_id=1 OR type_id=2;
+SELECT name, price FROM products WHERE type_id=(SELECT id FROM types WHERE name = 'сыр') OR type_id=(SELECT id FROM types WHERE name = 'молоко');
 																								  
---7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.  
-SELECT types.name AS type FROM products, types WHERE (type_id=types.id AND quantity < 10) GROUP BY types.name;
+--7. Написать запрос, который выводит тип продуктов, которых осталось меньше 3 штук ( 3 вместо 10 для наглядности).
+SELECT types.name AS type FROM products, types WHERE (SELECT COUNT(*) FROM PRODUCTS WHERE type_id=types.id) < 3 GROUP BY types.name;
 																								  
 --8. Вывести все продукты и их тип.
 SELECT products.name AS name, types.name AS type FROM products, types WHERE type_id=types.id;
