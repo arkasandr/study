58bf50d6effac7accc7a4236e0e030e3c582800e
arkasandr <arkasandr@gmail.com>
2019-07-24 22:06:03 +0300

    Парсер вакансий на sql.ru [#1731]

---
+++ chapter_007/src/main/java/ru/job4j/parsersql/MainParser.java
@@ -0,0 +1,25 @@
+package ru.job4j.parsersql;
+
+import ru.job4j.parsersql.Config;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+     * @author Alex Arkashev (arkasandr@gmail.com)
+     * @version $Id$
+     * @since 0.1
+     */
+
+    public class MainParser {
+
+        public static void main(String[] args) throws ClassNotFoundException, SQLException {
+            Config conf = new Config();
+            WorkParser workParser = new WorkParser(conf);
+
+            workParser.connectDB();
+          workParser.generate(10);
+
+
+        }
+}
---
+++ chapter_007/src/main/java/ru/job4j/parsersql/WorkParser.java
@@ -0,0 +1,211 @@
+package ru.job4j.parsersql;
+
+import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alex Arkashev (arkasandr@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class WorkParser implements AutoCloseable {
+
+
+    private final Config config;
+    private Connection connection;
+    private static final String TABLE = "vacancy";
+    private static final String DATABASE = "vacancies.db";
+
+    public WorkParser(Config config) {
+        this.config = config;
+        this.config.init();
+    }
+
+    /**
+     * Метод осущствляет подключение к базе данных
+     * @param
+     * @return
+     * @throws
+     */
+    public boolean connectDB() throws ClassNotFoundException, SQLException {
+        Class.forName(config.get("driver-class-name"));
+        this.connection = DriverManager.getConnection(
+                config.get("url"),
+                config.get("username"),
+                config.get("password")
+        );
+        checkDatabaseExist();
+        if (!checkSchemaExist()) {
+            System.out.println("Table doesn't exist");
+            createSchema();
+           showSchema();
+        }
+        showSchema();
+        return this.connection != null;
+    }
+
+
+    public void generate(int size) {
+        String insertIntoSchema = "INSERT INTO " + TABLE
+                +
+                " (vacancy_name)" + " VALUES (?)";
+        try (
+                PreparedStatement ps = connection.prepareStatement(insertIntoSchema)) {
+            connection.setAutoCommit(false);
+            for (int i = 1; i <= size; i++) {
+                ps.setInt(1, i);
+                ps.addBatch();
+                showSchema();
+             //   System.out.println(i);
+            }
+            ps.executeBatch();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Метод определяет, существует ли необходимая база данных
+     * @param
+     * @return
+     * @throws
+     */
+    private boolean checkDatabaseExist() {
+        boolean result = false;
+        File file = new File(DATABASE);
+        if (file.exists()) {
+            System.out.println("DATABASE is exists!");
+            result = true;
+        } else {
+            System.out.println("You need create DATABASE!");
+            createNewDatabase();
+        }
+        return result;
+    }
+
+    /**
+     * Метод создает необходимую базу данных
+     * @param
+     * @return
+     * @throws
+     */
+    private static void createNewDatabase() {
+        String url = "jdbc:sqlite:" + DATABASE;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Метод проверяет наличие необходимой схемы в базе данных
+     * @param
+     * @return
+     * @throws
+     */
+    private boolean checkSchemaExist() {
+        boolean result = false;
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet rs = metaData.getTables(null, null, "%", null);
+            while (rs.next()) {
+                if (rs.getString("TABLE_NAME").equals(TABLE)) {
+                    System.out.println("Table exists!");
+                    result = true;
+                }
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Метод создает необходимую схему в базе данных
+     * @param
+     * @return true
+     * @throws SQLException
+     */
+    private boolean clearSchema() {
+        boolean result = false;
+        String clearSchema = "DELETE FROM " + TABLE;
+        try (
+                PreparedStatement ps = connection.prepareStatement(clearSchema)) {
+            ps.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Метод создает необходимую схему в базе данных
+     * @param
+     * @return true
+     * @throws SQLException
+     */
+    private boolean showSchema() {
+        boolean result = false;
+        String showSchema = "SELECT * FROM " + TABLE;
+        List<Integer> fields = new ArrayList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet rs = st.executeQuery(showSchema)) {
+            while (rs.next()) {
+                int field = rs.getInt("vacancy_name");
+                System.out.println("vacancy name =" + field);
+                fields.add(field);
+                result = true;
+            }
+            int size = fields.size();
+            System.out.println(size);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * Метод очищает необходимую схему в базе данных
+     * @param
+     * @return true
+     * @throws SQLException
+     */
+    private boolean createSchema() {
+        boolean result = false;
+        String createSchema = "CREATE TABLE IF NOT EXISTS " + TABLE + " ("
+                + "vacancy_name INTEGER NOT NULL "
+                + ")";
+        try (
+                PreparedStatement ps = connection.prepareStatement(createSchema)) {
+            ps.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
